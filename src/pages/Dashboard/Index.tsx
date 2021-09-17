3d9c8c2b5d95d26f0d88128d06b5ca9d5e8d768e
Renan Rambul <renanrambuls@gmail.com>
2021-09-16 22:01:48 -0300

    dashbord style and api

--- src/pages/Dashboard/Index.tsx
+++ src/pages/Dashboard/Index.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api'
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg'
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepostories] = useState([]);
+
+  function handleAddRepository() {
+    console.log(newRepo)
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input
+        value={newRepo}
+        onChange={(e) => setNewRepo(e.target.value)}
+        placeholder="Digite o nome do repositório aqui"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+          src=""
+          alt="RENAN URSO" />
+        <div>
+          <strong>Renan ursao</strong>
+          <p>Easy Peasy Highly scalable ReactJS & React Native Forms!</p>
+        </div>
+
+        <FiChevronRight size={20}/>
+        </a>
+
+      </Repositories>
+    </>
+    );
 };
 
 export default Dashboard
